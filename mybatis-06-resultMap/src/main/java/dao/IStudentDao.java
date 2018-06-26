7c857aeaa9aaa03e864e8a0134b13c91a2a11af5
Damaer <976170778@qq.com>
2018-06-26 16:28:21 +0800

    修改resultmap 的demo

--- mybatis-06-resultMap/src/main/java/dao/IStudentDao.java
+++ mybatis-06-resultMap/src/main/java/dao/IStudentDao.java
@@ -6,25 +6,9 @@ import java.util.List;
 import java.util.Map;
 
 public interface IStudentDao {
-    // 增加学生
-    public void insertStudent(Student student);
-    // 增加新学生并返回id
-    public void insertStudentCacheIdNoReturn(Student student);
-    // 增加新学生并返回id返回result
-    public int insertStudentCacheId(Student student);
-
-    // 根据id删除学生
-    public void deleteStudentById(int id);
-    // 更新学生的信息
-    public void updateStudent(Student student);
-
     // 返回所有学生的信息List
     public List<Student> selectAllStudents();
-    // 返回所有学生的信息Map
-    public Map<String, Object> selectAllStudentsMap();
-
     // 根据id查找学生
     public Student selectStudentById(int id);
-    // 根据名字查找学生，模糊查询
-    public List<Student>selectStudentsByName(String name);
+
 }
--- mybatis-06-resultMap/src/test/java/MyTest.java
+++ mybatis-06-resultMap/src/test/java/MyTest.java
@@ -13,60 +13,6 @@ public class MyTest {
 	public void Before(){
 		dao=new StudentDaoImpl();
 	}
-    /**
-     * 插入测试
-     */
-    @Test
-    public void testInsert(){
-        /**
-         * 要是没有select id，这样是不会自动获取id的，id会一直为空
-         */
-        Student student=new Student("hello",14,94.6);
-        System.out.println("插入前：student="+student);
-        dao.insertStudent(student);
-        System.out.println("插入后：student="+student);
-    }
-    /**
-     * 测试插入后获取id
-     */
-    @Test
-    public void testinsertStudentCacheIdNoReturn(){
-        Student student=new Student("helloworld",17,101);
-        System.out.println("插入前：student="+student);
-        dao.insertStudentCacheIdNoReturn(student);
-        System.out.println("插入后：student="+student);
-    }
-    /**
-     * 测试插入后获取id
-     */
-    @Test
-    public void testinsertStudentCacheId(){
-        Student student=new Student("helloworld",17,101);
-        System.out.println("插入前：student="+student);
-        int result = dao.insertStudentCacheId(student);
-        System.out.println("result:"+result);
-        System.out.println("插入后：student="+student);
-    }
-    /*
-     * 测试删除
-     *
-     */
-    @Test
-    public void testdeleteStudentById(){
-        dao.deleteStudentById(5);
-
-    }
-    /*
-     * 测试修改，一般我们业务里面不这样写，一般是查询出来student再修改
-     *
-     */
-    @Test
-    public void testUpdate(){
-        Student student=new Student("lallalalla",14,94.6);
-        student.setId(21);
-        dao.updateStudent(student);
-
-    }
     /*
      * 查询列表
      *
@@ -81,17 +27,6 @@ public class MyTest {
         }
     }
     /*
-     * 查询列表装成map
-     *
-     */
-    @Test
-    public void testselectMap(){
-        Map<String,Object> students=dao.selectAllStudentsMap();
-        // 有相同的名字的会直接替换掉之前查出来的，因为是同一个key
-        System.out.println(students.get("helloworld"));
-        System.out.println(students.get("1ADAS"));
-    }
-    /*
      * 通过id来查询student
      *
      */
@@ -100,17 +35,5 @@ public class MyTest {
         Student student=dao.selectStudentById(1);
         System.out.println(student);
     }
-    /*
-     * 通过模糊查询student的名字
-     *
-     */
-    @Test
-    public void testselectStudentByName(){
-        List<Student>students=dao.selectStudentsByName("abc");
-        if(students.size()>0){
-            for(Student student:students)
-                System.out.println(student);
-        }
 
-    }
 }
