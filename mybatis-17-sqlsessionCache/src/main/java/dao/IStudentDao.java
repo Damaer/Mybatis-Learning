8515b7eb7af56655ba40bfd6f325bc811fb04616
Damaer <976170778@qq.com>
2018-07-17 20:44:07 +0800

    修改demo

--- mybatis-17-sqlsessionCache/src/main/java/dao/IStudentDao.java
+++ mybatis-17-sqlsessionCache/src/main/java/dao/IStudentDao.java
@@ -5,15 +5,6 @@ import java.util.Map;
 import beans.Student;
 
 public interface IStudentDao {
-	public void insertStudent(Student student);
-	public void insertStudentCacheId(Student student);
-	
-	public void deleteStudentById(int id);
-	public void updateStudent(Student student);
-	
-
-	public List<Student> selectAllStudents();
 	public Student selectStudentById(int id);
-	public List<Student>selectStudentsByName(String name);
-	public List<Student>selectStudentByNameAndAge(Map<String, Object> map);
+
 }
--- mybatis-17-sqlsessionCache/src/test/java/MyTest.java
+++ mybatis-17-sqlsessionCache/src/test/java/MyTest.java
@@ -18,77 +18,18 @@ public class MyTest {
 		sqlSession=MyBatisUtils.getSqlSession();
 		dao=sqlSession.getMapper(IStudentDao.class);
 	}
-
-	@Test
-	public void testInsert(){
-
-		Student student=new Student("enasden",21,94.6);
-		System.out.println("student="+student);
-		dao.insertStudent(student);
-		System.out.println("student="+student);
-
-		sqlSession.commit();
-	}
-	@Test
-	public void testinsertStudentCacheId(){
-		Student student=new Student("helloworld",14,94.6);
-		System.out.println("student="+student);
-		dao.insertStudentCacheId(student);;
-		System.out.println("student="+student);
-	}
-	@Test
-	public void testdeleteStudentById(){
-		dao.deleteStudentById(20);
-
-	}
-	@Test
-	public void testUpdate(){
-		Student student=new Student("lallalalla",14,94.6);
-		student.setId(1);
-		dao.updateStudent(student);
-
-	}
-
-	@Test
-	public void testselectList(){
-		List<Student>students=dao.selectAllStudents();
-		if(students.size()>0){
-			for(Student student:students){
-				System.out.println(student);
-			}
-		}
-	}
+	
 	@Test
 	public void testselectStudentById(){
+
+		// 第一次查询
 		Student student=dao.selectStudentById(1);
 		System.out.println(student);
+		// 第二次查询
+		Student student2=dao.selectStudentById(1);
+		System.out.println(student2);
 	}
 
-	@Test
-	public void testselectStudentByName(){
-		List<Student>students=dao.selectStudentsByName("hello");
-		if(students.size()>0){
-			for(Student student:students)
-				System.out.println(student);
-		}
-		
-	}
-	
-	
-	@Test
-	public void testselectStudentByNameAndAge(){
-		Student stu=new Student("lallal", 1212, 40);
-		Map<String,Object>map=new HashMap<String, Object>();
-		map.put("nameCon", "abc");
-		map.put("ageCon", 14);
-		map.put("stu", stu);
-		List<Student>students=dao.selectStudentByNameAndAge(map);
-		if(students.size()>0){
-			for(Student student:students)
-				System.out.println(student);
-		}
-		
-	}
 	@After
 	public void after(){
 		if(sqlSession!=null){
